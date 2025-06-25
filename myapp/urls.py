233f2a845d786c04f83715f766f1daa86074df69
Ananda Septian Firdaus <apple@Nans.local>
2025-06-25 10:38:22 +0700

    penambahan f_newongoing_stunting_ringkasanv3

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import f_newrekap_stunting_vmp8, f_newrekap_stunting_dtl_vmp8, ong_stunting_dtl_regc, ong_stunting_v3_regc, ong_stunting_genting_allreg, ong_stunting_genting_ringkasan_allreg, f_newrekap_stunting_ringkasan_all
+from .views import f_newrekap_stunting_vmp8, f_newrekap_stunting_dtl_vmp8, ong_stunting_dtl_regc, ong_stunting_v3_regc, ong_stunting_genting_allreg, ong_stunting_genting_ringkasan_allreg, f_newrekap_stunting_ringkasan_all, ong_stunting_ringkasan_v3_allreg
 
 urlpatterns = [
     path('newrekap-stunting', f_newrekap_stunting_vmp8, name='newrekap_stunting'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('stunting-v3', ong_stunting_v3_regc, name='stunting_v3'),
     path('stunting-genting', ong_stunting_genting_allreg, name='stunting_genting_allreg'),
     path('stunting-genting-ringkasan', ong_stunting_genting_ringkasan_allreg, name='stunting_genting_ringkasan_allreg'),
+    path('stunting-ringkasan', ong_stunting_ringkasan_v3_allreg, name='stunting_ringkasan_v3_allreg'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -29,6 +29,10 @@ from .f_newongoing_stunting_genting_ringkasan_regb import stunting_genting_ringk
 from .f_newongoing_stunting_genting_ringkasan_rega import stunting_genting_ringkasan_rega
 from .f_newongoing_stunting_genting_ringkasan_regc import stunting_genting_ringkasan_regc
 from .f_newongoing_stunting_genting_ringkasan_regd import stunting_genting_ringkasan_regd
+from .f_newongoing_stunting_ringkasan_v3_rega import stunting_ringkasan_v3_rega
+from .f_newongoing_stunting_ringkasan_v3_regb import stunting_ringkasan_v3_regb
+from .f_newongoing_stunting_ringkasan_v3_regc import stunting_ringkasan_v3_regc
+from .f_newongoing_stunting_ringkasan_v3_regd import stunting_ringkasan_v3_regd
 
 @extend_schema(
     methods=['GET'],
@@ -550,6 +554,99 @@ async def ong_stunting_genting_ringkasan_allreg(request):
                     return JsonResponse({"data": data}, safe=False)
             if params['v_id_propinsi'] in {1, 5, 15, 20, 22, 19, 39, 37, 38, 4, 24, 3}:
                     results = await stunting_genting_ringkasan_regd(params)
+                    data = [dict(row) for row in results]
+                    return JsonResponse({"data": data}, safe=False)
+            else:
+                return JsonResponse({"error": f"Schema tidak ditemukan untuk id_provinsi = {params['v_id_propinsi']}"}, status=400)
+        except ValueError as e:
+            return JsonResponse({"error": str(e)}, status=400)
+        
+@extend_schema(
+    methods=['GET'],
+    parameters=[
+        OpenApiParameter(name='v_bulan', required=True, type=int),
+        OpenApiParameter(name='v_tahun', required=True, type=int),
+        OpenApiParameter(name='v_nik', required=False, type=str),
+        OpenApiParameter(name='v_nama', required=False, type=str),
+        OpenApiParameter(name='v_id_propinsi', required=True, type=int),
+        OpenApiParameter(name='v_id_kabupaten', required=True, type=int),
+        OpenApiParameter(name='v_id_kecamatan', required=True, type=int),
+        OpenApiParameter(name='v_id_kelurahan', required=True, type=int),
+        OpenApiParameter(name='v_id_rw', required=False, type=int),
+        OpenApiParameter(name='v_id_rt', required=False, type=str),
+        OpenApiParameter(name='v_balita', required=False, type=int),
+        OpenApiParameter(name='v_baduta', required=False, type=int),
+        OpenApiParameter(name='v_status_hamil', required=False, type=bool),
+        OpenApiParameter(name='v_status_pus', required=False, type=int),
+        OpenApiParameter(name='v_status_verval', required=False, type=bool),
+        OpenApiParameter(name='v_status_krs', required=False, type=int),
+        OpenApiParameter(name='v_nik_tidak_wajar', required=False, type=bool),
+        OpenApiParameter(name='v_status_keluarga', required=False, type=str),
+        OpenApiParameter(name='v_kesejahteraan_prioritas', required=False, type=str),
+        OpenApiParameter(name='v_flag_genting', required=False, type=int),
+        OpenApiParameter(name='v_sasaran_genting', required=False, type=str),
+        OpenApiParameter(name='v_sudah_diukur', required=False, type=bool),
+        OpenApiParameter(name='v_status_pengukuran', required=False, type=bool),
+        OpenApiParameter(name='verval_ya', required=False, type=int),
+    ],
+    responses={200: dict},
+)
+@api_view(['GET'])        
+@permission_classes([AllowAny])
+@csrf_exempt
+@async_to_sync
+async def ong_stunting_ringkasan_v3_allreg(request):
+    if request.method == 'GET':
+        params = {
+            'v_bulan': request.GET.get('v_bulan', None),
+            'v_tahun': request.GET.get('v_tahun', None),
+            'v_nik': request.GET.get('v_nik', None),
+            'v_nama': request.GET.get('v_nama', None),
+            'v_id_propinsi': request.GET.get('v_id_propinsi', None),
+            'v_id_kabupaten': request.GET.get('v_id_kabupaten', None),
+            'v_id_kecamatan': request.GET.get('v_id_kecamatan', None),
+            'v_id_kelurahan': request.GET.get('v_id_kelurahan', None),
+            'v_id_rw': request.GET.get('v_id_rw', None),
+            'v_id_rt': request.GET.get('v_id_rt', None),
+            'v_balita': request.GET.get('v_balita', None),
+            'v_baduta': request.GET.get('v_baduta', None),
+            'v_status_hamil': request.GET.get('v_status_hamil', None),
+            'v_status_pus': request.GET.get('v_status_pus', None),
+            'v_status_verval': request.GET.get('v_status_verval', None),
+            'v_status_krs': request.GET.get('v_status_krs', None),
+            'v_nik_tidak_wajar': request.GET.get('v_nik_tidak_wajar', None),
+            'v_status_keluarga': request.GET.get('v_status_keluarga', None),
+            'v_kesejahteraan_prioritas': request.GET.get('v_kesejahteraan_prioritas', None),
+            'v_flag_genting': request.GET.get('v_flag_genting', None),
+            'v_sasaran_genting': request.GET.get('v_sasaran_genting', None),
+            'v_sudah_diukur': request.GET.get('v_sudah_diukur', None),
+            'v_status_pengukuran': request.GET.get('v_status_pengukuran', None),
+            'verval_ya': request.GET.get('verval_ya', None),
+        }
+        # params['id_provinsi'] = params['v_id_propinsi']
+
+        try:
+            if params['v_tahun'] is not None:
+                params['v_tahun'] = int(params['v_tahun'])
+            if params['v_bulan'] is not None:
+                params['v_bulan'] = int(params['v_bulan'])
+            if params['v_id_propinsi'] is not None:
+                params['v_id_propinsi'] = int(params['v_id_propinsi'])
+
+            if params['v_id_propinsi'] in {11, 13, 14, 17}:
+                    results = await stunting_ringkasan_v3_regc(params)
+                    data = [dict(row) for row in results]
+                    return JsonResponse({"data": data}, safe=False)
+            if params['v_id_propinsi'] in {9, 16, 7, 28, 21, 23, 36, 10, 8, 30, 31, 33, 40, 32, 29, 26, 25, 27, 6, 2}:
+                    results = await stunting_ringkasan_v3_rega(params)
+                    data = [dict(row) for row in results]
+                    return JsonResponse({"data": data}, safe=False)
+            if params['v_id_propinsi'] in {12, 18}:
+                    results = await stunting_ringkasan_v3_regb(params)
+                    data = [dict(row) for row in results]
+                    return JsonResponse({"data": data}, safe=False)
+            if params['v_id_propinsi'] in {1, 5, 15, 20, 22, 19, 39, 37, 38, 4, 24, 3}:
+                    results = await stunting_ringkasan_v3_regd(params)
                     data = [dict(row) for row in results]
                     return JsonResponse({"data": data}, safe=False)
             else:
